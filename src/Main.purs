40dd10e44fa902a5e18a533e42bf505edd72984e
Daniel Harvey <daniel.harvey@habito.com>
2019-05-29 13:46:27 +0100

    Export all types explicitly

--- src/Main.purs
+++ src/Main.purs
@@ -1,3 +1,7 @@
-module Radox.React (module Connect) where
+module Radox.React (module Connect, module Provider, module Consumer, module
+  Types) where
 
-import Radox.React.Internal.Connect as Connect
+import Radox.React.Internal.Connect (createRadoxContext) as Connect
+import Radox.React.Internal.Provider (radoxProvider) as Provider
+import Radox.React.Internal.Consumer (radoxConsumer) as Consumer
+import Radox.React.Internal.Types (ReactRadoxContext, ReactRadoxRenderMethod) as Types
